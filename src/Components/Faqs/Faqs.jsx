874fd2d00ce0775e727d091493f1a42f9a402b13
adamabu578 <abukamsulum01@gmail.com>
2024-12-13 12:56:53 +0100

    faq title

--- src/Components/Faqs/Faqs.jsx
+++ src/Components/Faqs/Faqs.jsx
@@ -4,7 +4,7 @@ import FAQ from '../FAQ2/FAQ2'
 const Faqs = () => {
   return (
     <div className="text-center mt-20 lg:mt-40">
-      <h1 className="text-[40px] font-bold text-[#1B1F23] leading-[67.] ">Frequently Asked Questions</h1>
+      <h1 className="text-[40px] font-bold text-[#1B1F23] ">Frequently Asked Questions</h1>
       <p className='text-sm text-[#1B1F23] '>Want to know more about our services? Our FAQs section is here to help you with useful information about us.</p>
       <FAQ />
     </div>
