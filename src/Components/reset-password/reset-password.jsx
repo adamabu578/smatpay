37ba851331d675531d9e718006da7c6cf033fde1
adamu578 <abukamsulum01@gmail.com>
2025-07-23 22:18:05 +0100

    forgot-password

--- src/Components/reset-password/reset-password.jsx
+++ src/Components/reset-password/reset-password.jsx
@@ -39,7 +39,7 @@ export default function ResetPasswordPage() {
     }
     setIsSubmitting(true);
     try {
-      const response = await fetch("https://smatpay.live/api/reset-password", {
+      const response = await fetch("https://smatpay.live/api/forgot-password", {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
